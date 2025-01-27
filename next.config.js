d6d05b5f37c231ea0174a0fd335e78370d20c6a2
David Silva <me@zgrav.pro>
2025-01-27 13:03:47 +0100

    clean2

--- next.config.js
+++ next.config.js
@@ -1,29 +1,9 @@
-const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  productionBrowserSourceMaps: false,
-  staticPageGenerationTimeout: 1000 * 60 * 5,
-  poweredByHeader: false,
   generateBuildId: async () => {
-    return "local31";
+    return "local38";
   },
-  deploymentId: "local31",
-  experimental: {
-    instrumentationHook: true,
-    forceSwcTransforms: true,
-    esmExternals: false,
-    externalDir: true,
-    outputFileTracingRoot: path.join(__dirname),
-    isrFlushToDisk: false,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    disableStaticImages: true,
-    unoptimized: true,
-  },
-  optimizeFonts: true,
+  deploymentId: "local38",
   output: "standalone",
 };
 
